b7c6b7e5046bd66252c43da1af5024464b2ab809
Gauravchandola2022 <gauravchandola65@gmail.com>
2025-10-29 20:10:03 +0530

    edit keys

--- app.py
+++ app.py
@@ -3,9 +3,9 @@
 Project Samarth - Intelligent Q&A System for Indian Agricultural and Climate Data
 A Streamlit-based chat interface for querying data.gov.in datasets with AI-powered analysis
 """
-from dotenv import load_dotenv
-load_dotenv()
-print("✅ dotenv loaded successfully")
+# from dotenv import load_dotenv
+# load_dotenv()
+
 import streamlit as st
 import os
 from query_orchestrator import QueryOrchestrator
